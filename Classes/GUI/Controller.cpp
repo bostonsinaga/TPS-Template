53f597539247502cdb23b6745f677cb03d0faea3
bostonsinaga <bostonsinga@gmail.com>
2023-02-17 11:10:45 +0700

    setup controller base

--- Classes/GUI/Controller.cpp
+++ Classes/GUI/Controller.cpp
@@ -5,3 +5,34 @@
 #include "Controller.h"
 
 USING_NS_CC;
+
+Controller::Controller() {
+    pad = NStool::createSpriteFrame(
+        "",
+        NStool::edgeSpawnVec2_SouthWest
+    );
+
+    stick[0] = NStool::createSpriteFrame(
+        "",
+        pad->getPosition();
+    );
+
+    stick[1] = NStool::createSpriteFrame(
+        "",
+        pad->getPosition();
+    );
+
+    stick[1]->setVisible(false);
+}
+
+void Controller::touchBegan(const std::vector<Touch*> &touches) {
+
+}
+
+void Controller::touchMoved(const std::vector<Touch*> &touches) {
+
+}
+
+void Controller::touchEnded(const std::vector<Touch*> &touches) {
+
+}
--- Classes/GUI/Controller.h
+++ Classes/GUI/Controller.h
@@ -9,9 +9,16 @@
 
 class Controller {
 public:
+    Controller();
+
+    void touchBegan(const std::vector<Touch*> &touches);
+    void touchMoved(const std::vector<Touch*> &touches);
+    void touchEnded(const std::vector<Touch*> &touches);
 
 private:
-
+    cocos2d::Sprite
+        *pad,
+        *stick[2]; // [0] = normal, [1] = pressed
 };
 
 #endif // __CONTROLLER_H__
